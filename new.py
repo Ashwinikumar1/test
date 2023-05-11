2949c765f436caafba702bb3fa2e11b096f8a898
Ashwinikumar1 <ashwinikumar081@gmail.com>
2023-05-11 16:55:30 +0530

    k

--- new.py
+++ new.py
@@ -81,3 +81,108 @@ def create_new_cv_data(cv_count):
                         df = df.loc[:, df.columns.str.lower().isin(fhir_table_mapping[fhir_table_mapping['Table_Name'] == table_name ]["Column_Name"].tolist())]
                         print("mapped_data_count", df.shape[1])
                         data_count.append(df.shape[1])
+                        
+                        
+                        
+
+import os
+import glob
+import random
+import pandas as pd
+
+
+def file_list_generator(input_files_list, input_data_folder_path, data_read_dict):
+    """
+    Generates a list of files from given input_files_list and input_data_folder_path.
+
+    Args:
+        input_files_list (list): A list of folder names.
+        input_data_folder_path (str): The path to the input data folder.
+        data_read_dict (dict): A dictionary of folder names and their read status.
+
+    Returns:
+        list: A list of files along with their file names, in the following format:
+            [[file_path, file_name, folder_name], [file_path, file_name, folder_name], ...]
+    """
+    all_files = []
+    for folder_name in input_files_list:
+        if folder_name in data_read_dict.keys():
+            if data_read_dict[folder_name] == "file_read":
+                # If the folder has been read before
+                file_list = glob.glob(os.path.join(input_data_folder_path, folder_name, "*.csv"))
+                file_list1 = glob.glob(os.path.join(input_data_folder_path, folder_name, "*.parquet"))
+                total_list = file_list + file_list1
+                random.shuffle(total_list)
+
+                # Add each file to the all_files list along with its file name and folder name
+                for x in total_list:
+                    all_files.append([x, x.split("/")[-1][:-4].lower(), folder_name])
+
+            else:
+                # If the folder has not been read before
+                folder_list = os.listdir(os.path.join(input_data_folder_path, folder_name))
+                for i in folder_list:
+                    # Look for CSV and Parquet files in the subfolders of the current folder
+                    file_paths_csv = glob.glob(os.path.join(input_data_folder_path, folder_name, i, "*.csv"))
+                    file_paths_csv1 = glob.glob(os.path.join(input_data_folder_path, folder_name, i, "*", "*.csv"))
+                    file_paths_parquet = glob.glob(os.path.join(input_data_folder_path, folder_name, i, "*.parquet"))
+                    file_paths_parquet1 = glob.glob(os.path.join(input_data_folder_path, folder_name, i, "*", "*.parquet"))
+                    file_paths_parquet_combined = file_paths_parquet + file_paths_parquet1
+                    random.shuffle(file_paths_parquet_combined)
+
+                    # Read each Parquet file until one with at least 500 rows is found
+                    for ind, f in enumerate(file_paths_parquet_combined):
+                        temp = pd.read_parquet(f)
+                        if temp.shape[0] >= 500:
+                            file_paths_parquet_combined = [file_paths_parquet_combined[ind]]
+                            break
+                        else:
+                            continue
+
+                    # Combine all file paths and choose one at random
+                    filename_list = file_paths_csv + file_paths_csv1 + file_paths_parquet_combined
+                    if len(filename_list) > 0:
+                        random.shuffle(filename_list)
+                        all_files.append([filename_list[0], i.lower(), folder_name])
+
+    return all_files
+
+import os
+import glob
+import random
+import pandas as pd
+
+def file_list_generator(input_data_folder_path):
+    """
+    This function generates a list of files in the specified input folder path and its subdirectories.
+    The files are randomly shuffled and stored in a list of lists, with each inner list containing:
+    [file path, lowercase filename without extension, parent folder name]
+
+    Args:
+    - input_data_folder_path: string, the path to the input data folder
+
+    Returns:
+    - all_files: list of lists, where each inner list contains the file path, lowercase filename without extension, 
+      and parent folder name
+    """
+
+    # Create a list of all the subdirectories and files in the input folder path
+    input_files_list = os.listdir(input_data_folder_path)
+    all_files = []
+
+    # Loop through each item in the input folder path
+    for folder_name in input_files_list:
+
+        # Find all the files with a .csv or .parquet extension in the current subdirectory
+        file_list = glob.glob(os.path.join(input_data_folder_path,folder_name,"*.csv"))
+        file_list1 = glob.glob(os.path.join(input_data_folder_path,folder_name,"*.parquet"))
+        total_list = file_list + file_list1
+
+        # Randomly shuffle the list of files
+        random.shuffle(total_list)
+
+        # Append each file path, lowercase filename without extension, and parent folder name to the all_files list
+        for x in total_list:
+            all_files.append([x,x.split("/")[-1][:-4].lower(),folder_name])
+
+    return all_files
